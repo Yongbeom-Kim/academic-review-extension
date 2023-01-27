d75f5bf2e1c50378a157808f124115dd50458a7f
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-27 16:11:57 +0800

    simplify esbuild script

--- scripts/esbuild.mjs
+++ scripts/esbuild.mjs
@@ -1,23 +1,15 @@
 
 import * as esbuild from 'esbuild'
-import {getFiles} from './file_utils.mjs'
 
-const src_path = './src'
-const dist_path = './dist'
-
-const entryPoints = getFiles(src_path).map(path => {return {in: path, out: path.replace(src_path, "")}})
-
-console.log(`Building:`)
-entryPoints.forEach((x) => console.log('> ' + x.in));
+// build entries
+const entryPoints = [
+    {in: 'src/content_script/inject_html.tsx', out: 'content_script/inject_html.js'}
+]
 
 esbuild.build({
-  entryPoints,
-  bundle: true,
-  write: true,
-  outdir: dist_path,
-  format: 'iife'
+    entryPoints,
+    bundle: true,
+    write: true,
+    outdir: './dist',
+    format: 'iife'
 })
-
-// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-// print green yay
-console.info("\x1b[32m%s\x1b[0m", "\nBuild Done")
