c639e5c32af985037cdc09654d6412015e5dcc1e
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-27 11:39:59 +0800

    initial commit/basic plugin structure

---
+++ src/popup/main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return ( 
+        <main>
+            <h1>Hello World! (Popup)</h1>
+        </main>
+    )
+}
---
+++ src/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './main'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Main></Main>
+  </React.StrictMode>,
+)
