859fd33adae6846f262d32056a9ac04db55439dc
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-27 18:20:39 +0800

    Script to find highest z-index doesn't work, kiv for later

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { getMaxZIndex } from '../content_script/utils/find-highest-z-index';
 import styles from './Sidebar.module.css';
 
 
 export default function Sidebar() {
     const [shown, setShown] = useState(true);
 
+    // TODO: Calculate max z value and be greater than that
+    // const [maxZIndex, setMaxZIndex] = useState(0);
+    // useEffect(() => {
+    //     setMaxZIndex(useMemo(() => getMaxZIndex(), []))
+    // }, []);
+    
     useEffect(() => {
-        setShown(true)
-        console.log("shown is" + shown ? "true" : "false");
+        setShown(true);
     }, []);
+
+    
+    
 
     return (
         <>
@@ -24,7 +33,7 @@ export default function Sidebar() {
                 <h1>Header1</h1>
                 <h2>Header2</h2>
                 <h3>Header3</h3>
-            </div> : <></>}
+            </div> : <div>false</div>}
         </>
     )
 }
