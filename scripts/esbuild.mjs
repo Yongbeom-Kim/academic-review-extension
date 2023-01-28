6f86acad5e8242933e7769b73792b6df8b6423cd
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-29 02:56:25 +0800

    Adjust build script + create files for extension page

--- scripts/esbuild.mjs
+++ scripts/esbuild.mjs
@@ -2,20 +2,35 @@ import fs, { watch } from 'fs';
 import * as esbuild from 'esbuild'
 import CssModulesPlugin from 'esbuild-css-modules-plugin';
 
+const OUT_DIR = './dist/'
+
 // build entries
 const entryPoints = [
+    // extension
     {in: 'src/extension/content_script/inject_html.tsx', out: 'content_script/inject_html'},
     {in: 'src/extension/background_script/detect_browser_action.ts', out: 'background_script/detect_browser_action'},
+
+    // extension page
+    {in: 'src/extension_page/index.tsx', out: 'extension_page/index'}
+]
+
+const toCopy = [
+    //static files
+    {in: 'static/', out: '/'},
+
+    //extension page
+    {in: 'src/extension_page/index.html', out: 'extension_page/index.html'}
 ]
 
 esbuild.build({
     entryPoints,
     bundle: true,
     write: true,
-    outdir: './dist',
+    outdir: OUT_DIR,
     format: 'iife',
     plugins: [CssModulesPlugin()]
 })
 
-// copy static files
-fs.cpSync('static/', 'dist/', {recursive: true, force: true})
+toCopy.forEach((x) => {
+    fs.cpSync(x.in, OUT_DIR+x.out, {recursive: true})
+})
---
+++ src/extension_page/index.tsx
@@ -0,0 +1,9 @@
+import * as ReactDOM from 'react-dom/client';
+import { } from 'react';
+import React from 'react';
+
+ReactDOM.createRoot(document.getElementById('root') as Element).render(
+    <>
+        <h1>Hello World!</h1>
+    </>
+)
