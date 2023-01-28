48f99b06bf937fed61657e51f1100c234b4a2b9d
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-28 12:09:09 +0800

    add 2 unresponsive url buttons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,6 @@ import browser from 'webextension-polyfill';
 // import { getMaxZIndex } from '../content_script/utils/find-highest-z-index';
 import styles from './Sidebar.module.css';
 
-
 export default function Sidebar() {
     const [shown, setShown] = useState(false);
     const toggleShown = () => setShown(shown => !shown);
@@ -30,10 +29,17 @@ export default function Sidebar() {
             {shown ? <div className={`${styles.sidebar}`}>
                 <button onClick={e => toggleShown()} >
                     Hide</button>
-                <h1>Header1</h1>
-                <h2>Header2</h2>
-                <h3>Header3</h3>
+                <UrlButtons />
             </div> : <div>false</div>}
         </>
+    )
+}
+
+function UrlButtons() {
+    return (
+        <div className={`${styles.urlButtons}`}>
+            <button>Add current page</button>
+            <button>Find links in page</button>
+        </div>
     )
 }
