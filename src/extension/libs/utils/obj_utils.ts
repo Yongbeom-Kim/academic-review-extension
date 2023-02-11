d1d399ba8cef901be140130256ef351822fdbc59
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-02-12 00:33:29 +0800

    replace objectArrayToCsv with DataFrame method

--- src/extension/libs/utils/obj_utils.ts
+++ src/extension/libs/utils/obj_utils.ts
@@ -1,3 +1,5 @@
+import { DataFrame } from "../../model/DataFrame"
+
 /**
  * Converts an array of plain objects to csv format
  * Assumes that each field of the object array is a string (or likewise primitive)
@@ -5,18 +7,7 @@
  * @returns csv string
  */
 export function objArrayToCsv(oa: Record<any, any>[]): string {
-    let headers = Object.keys(oa[0])
-
-    let str = headers.map(x => `"${x}"`).join(",");
-    str += "\n";
-    str += oa
-        .map(o => Object.values(o)
-            .map(x => `${x}`.replaceAll('"', '""')) // Replace " with "" to avoid csv parse errors
-            .map(x => x.length === 9 ? '' : '"' + x + '"')
-            .join(','))
-        .join('\n');
-
-    return str;
+    return DataFrame.FromPlainObject(oa).toCsvString();
 }
 
 const UNDEFINED_STR = '__undefined__'
