be1f67d9250940a69c5b991c9c56470db0356bca
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-02-20 12:24:08 +0800

    clean up pdf_parser.ts

--- src/extension/libs/pdf_parser.ts
+++ src/extension/libs/pdf_parser.ts
@@ -20,6 +20,18 @@ const KEYWORD_MIN_WORDS = 5;
 const ACKNOWLEDGEMENTS_SECTION_HEADER_REGEX = /^ACKNOWLEDGEMENTS?/i;
 const BIBLIOGRAPHY_SECTION_HEADER_REGEX = /^(?:LITERATURE CITED|CITATIONS?|REFERENCES?|BIBLIOGRAPHY)/i;
 
+/**
+ * Get the PDFDocumentProxy for a given local pdf url
+ * @param url that points to pdf
+ * @returns a Promise<PDFDocumentProxy>
+ */
+export async function getPdfProxy(url: string): Promise<PDFDocumentProxy> {
+    return getDocument(url).promise;
+}
+
+/**
+ * Class that represents a parsed PDFDocumentProxy object.
+ */
 export class ParsedPdf {
 
     paragraphs: string[][];
@@ -73,7 +85,6 @@ export class ParsedPdf {
         return this.getTextbyPages(page_number_array)
     }
     // getAuthorCountries
-    // getBody
 
     /**
      * Get a section by the section header regex, as well as the minimum number of words in the section.
@@ -147,79 +158,4 @@ export class ParsedPdf {
         const result = text_items_by_page.map(x => x.map(y => y.str))
         return new ParsedPdf(result);
     }
-}
-
-/**
- * Get the PDFDocumentProxy for a given local pdf url
- * @param url that points to pdf
- * @returns a Promise<PDFDocumentProxy>
- */
-export async function getPdfProxy(url: string): Promise<PDFDocumentProxy> {
-    return getDocument(url).promise;
-}
-
-// const KEYWORD_HEADER_REGEX = /Key\s*words\./i
-const KEYWORD_END_REGEX = /RAFFLES|INTRODUCTION/i
-
-export async function getKeywords(pdf: PDFDocumentProxy): Promise<string> {
-    // keyword is in the 1st page
-    const first_page = await pdf.getPage(1);
-    const text = await getTextInPage(first_page);
-
-    const keyword_start = text.search(KEYWORD_HEADER_REGEX)
-
-    const text_sliced_left = text.slice(keyword_start);
-    const keyword_end = text_sliced_left.search(KEYWORD_END_REGEX)
-
-    return text_sliced_left.slice(0, keyword_end);
-}
-
-
-
-
-
-
-/**
- * Get all the text in a given page.
- * There are no newlines in the returned text.
- * @param page PDFPageProxy to access
- * @returns all the text on the page.
- */
-export async function getTextInPage(page: PDFPageProxy): Promise<string> {
-    return getTextInPageByLine(page).then(s => s.replaceAll('\n', ' '));
-}
-
-/**
- * Get all the text in a given page, attempting to insert \n every line.
- * @param page PDFPageProxy to access
- * @returns all the text on the page.
- */
-export async function getTextInPageByLine(page: PDFPageProxy): Promise<string> {
-    let y_val = 0;
-    let text = '';
-    (await page.getTextContent()).items.forEach((item: TextItem | TextMarkedContent) => {
-        if (!('str' in item)) // assert item is TextItem
-            return;
-
-        if (typeof item.str === 'undefined')
-            return;
-
-        if (item.transform[5] !== y_val) {
-            text += '\n';
-            y_val = item.transform[5];
-        }
-        text += item.str;
-    })
-
-    return text;
-}
-
-export async function getText(pdf: PDFDocumentProxy): Promise<string> {
-    const pages = []
-    for (let i = 1; i <= pdf.numPages; i++) {
-        pages.push(await pdf.getPage(i))
-    }
-    const page_texts = await Promise.all(
-        pages.map(page => getTextInPage(page)))
-    return page_texts.join(' ');
 }
